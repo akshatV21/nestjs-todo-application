54815794f639c5508b0181977e4e8cdca2bd537f
akshatV21 <a.vishwakarma2110@gmail.com>
2022-10-25 21:00:26 +0530

    added user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,31 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose'
+import { hashSync } from 'bcrypt'
+import { Document } from 'mongoose'
+
+export type UserDocument = User & Document
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  username: string
+
+  @Prop({ required: true })
+  password: string
+
+  @Prop({ default: [], ref: Set })
+  sets: []
+
+  _doc: any
+}
+
+const UserSchema = SchemaFactory.createForClass(User)
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+
+  const hashedPassword = hashSync(this.password, 4)
+  this.password = hashedPassword
+  return next()
+})
+
+export { UserSchema }
