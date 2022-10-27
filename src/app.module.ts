053246dda9839265cebb7b09edeab8621cca7148
akshatV21 <a.vishwakarma2110@gmail.com>
2022-10-27 08:57:26 +0530

    added mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common'
 import { UserModule } from './user/user.module'
 import { SetsModule } from './sets/sets.module'
 import { TasksModule } from './tasks/tasks.module'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [UserModule, SetsModule, TasksModule],
+  imports: [
+    UserModule,
+    SetsModule,
+    TasksModule,
+    MongooseModule.forRoot('mongodb+srv://akshat21:aku1985pika@cluster0.ew0oz.mongodb.net/todo_app?'),
+  ],
   controllers: [],
   providers: [],
 })
